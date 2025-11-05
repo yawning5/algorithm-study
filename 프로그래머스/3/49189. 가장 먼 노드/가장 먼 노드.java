5792f256f9835cdcaf400fa8a3bdba7a80610249
yawning5 <yawning055@gmail.com>
2025-11-05 12:58:03 +0900

    [level 3] Title: 가장 먼 노드, Time: 24.48 ms, Memory: 102 MB -BaekjoonHub

--- 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.java
+++ 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.java
@@ -3,9 +3,17 @@ import java.util.*;
 class Solution {
     public int solution(int n, int[][] edge) {
         int answer = 0;
+        
+        Queue<Integer> q = new ArrayDeque<>();
+        boolean[] visited = new boolean[n + 1];
+        int[] dist = new int[n + 1];
+        visited[1] = true;
+        dist[1] = 0;
+        q.offer(1);
+        
         List<Integer>[] adj = new ArrayList[n + 1];
         for (int i = 1; i <= n; i++) {
-            adj[i] = new ArrayList<>();
+            adj[i]  = new ArrayList<>();
         }
         
         for (int[] e : edge) {
@@ -14,31 +22,26 @@ class Solution {
             adj[b].add(a);
         }
         
-        int[] dist = new int[n + 1];
-        Arrays.fill(dist, -1);
-    
-    Deque<Integer> q = new ArrayDeque<>();
-    q.offer(1); // 시작 노드
-    dist[1] = 0; // 시작 거리는 0
-    
-    while(!q.isEmpty()) {
-        int cur = q.poll();
-        for (int next : adj[cur]) {
-            if (dist[next] == -1) {
-                dist[next] = dist[cur] + 1;
-                q.offer(next);
+        while(!q.isEmpty()) {
+            int cur = q.poll();
+            
+            for (int next : adj[cur]) {
+                if (!visited[next]) {
+                    visited[next] = true;
+                    dist[next] = dist[cur] + 1;
+                    q.offer(next);
+                }
             }
         }
-    }
-    
+        
         int maxDist = 0;
-        for (int i = 1; i <= n; i++) {
+        for (int i = 1; i<= n; i++) {
             maxDist = Math.max(maxDist, dist[i]);
         }
         for (int i = 1; i <= n; i++) {
             if (dist[i] == maxDist) answer++;
         }
-    
-    return answer;
-}
+        
+        return answer;
+    }
 }
