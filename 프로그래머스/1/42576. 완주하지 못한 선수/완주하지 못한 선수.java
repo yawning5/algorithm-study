7f321a12bd42487f2cd96a52574d4c585124ac7d
yawning5 <yawning055@gmail.com>
2025-12-31 18:59:03 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 105.45 ms, Memory: 98 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -1,16 +1,24 @@
-import java.util.Arrays;
+import java.util.*;
 
 class Solution {
     public String solution(String[] participant, String[] completion) {
-Arrays.sort(participant);
-Arrays.sort(completion);
-
-for (int i = 0; i < completion.length; i++) {
-    if (!participant[i].equals(completion[i])) {
-        return participant[i];
-    }
-}
-
-return participant[participant.length - 1];
+        // 지원자 배열을 다 돌면서 K = 참가자 이름, V = 참가자 수 Map 을 만듬
+        Map<String, Integer> all = new HashMap<>();
+        
+        for (String p : participant) {
+            all.merge(p, 1, Integer::sum);
+        }
+        // 완료자 배열을 돌면서 V - 1 을 함
+        for (String c : completion) {
+            all.merge(c, -1, Integer::sum);
+        }
+        
+        // 완료자 배열을 돌면서 V != 0 인 참가자 이름을 뱉음
+        for (Map.Entry<String, Integer> entry : all.entrySet()) {
+            if (entry.getValue() != 0)
+                return entry.getKey();
+        }
+        
+        return null;
     }
 }
