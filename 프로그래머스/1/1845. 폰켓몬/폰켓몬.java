84b55c491b66539cc79177bd168a522892f4e326
yawning5 <yawning055@gmail.com>
2026-01-01 11:42:07 +0900

    [level 1] Title: 폰켓몬, Time: 6.81 ms, Memory: 74.6 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -1,18 +1,15 @@
-import java.util.HashMap;
-import java.util.Map;
-
+import java.util.*;
+import java.util.stream.*;
 
 class Solution {
     public int solution(int[] nums) {
-        int answer = 1;
-        Map<Integer, Integer> mon = new HashMap<>();
-
-        for (int i = 0; i < nums.length; i++) {
-            if (mon.containsKey(nums[i])) {
-                mon.put(nums[i], mon.get(nums[i]) + 1);
-            } else mon.put(nums[i], 1);
-        }
-
-        return Math.min(mon.size(), nums.length / 2);
+        // Set 으로해서 종류 파악
+        Set<Integer> types = 
+            Arrays.stream(nums)
+            .boxed()
+            .collect(Collectors.toSet());
+        
+        // 선택가능한 마릿수 와 존재하는 종류 수 중 더 작은 값
+        return Math.min(types.size(), nums.length / 2);
     }
 }
