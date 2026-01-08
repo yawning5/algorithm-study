8d1d5ce18aa66d4a422e0b0b4dc1e0b1a3150bb2
yawning5 <yawning055@gmail.com>
2026-01-09 00:03:48 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 68.97 ms, Memory: 120 MB -BaekjoonHub

--- 프로그래머스/1/12906. 같은 숫자는 싫어/README.md
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/README.md
@@ -1,10 +1,10 @@
 # [level 1] 같은 숫자는 싫어 - 12906 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12906) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12906?language=java) 
 
 ### 성능 요약
 
-메모리: 111 MB, 시간: 65.35 ms
+메모리: 120 MB, 시간: 68.97 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 04월 30일 08:05:06
+2026년 01월 09일 00:03:42
 
 ### 문제 설명
 
--- 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
@@ -1,19 +1,26 @@
 import java.util.*;
+import java.util.stream.*;
 
 public class Solution {
     public int[] solution(int []arr) {
-        Deque<Integer> deque = new ArrayDeque<>();
-        deque.add(arr[0]);
-        for(int i = 1; i < arr.length; i++) {
-            if (deque.peekLast() != arr[i]) {
-                deque.add(arr[i]);
+        List<Integer> answer = new ArrayList<>();
+        // 선입 선출 구조로 들어간 숫자를 변수에 저장한 뒤 다른 숫자가 오기 전까지 넣지 않고
+        Deque<Integer> q = new ArrayDeque<>();
+        // 다른 숫자가 오면 넣은 뒤 변수값 변경시키기
+        // 이후 큐 qoll 해서 answer 에 넣고 배열변환
+        int cur;
+        q.add(arr[0]);
+        for (int i : arr) {
+            cur = q.getLast();
+            if (cur != i) {
+                q.add(i);
             }
         }
-        int size = deque.size();
-        int[] answer = new int[size];
-        for(int i = 0; i < size; i++) {
-            answer[i] = deque.pollFirst();
+        while(!q.isEmpty()) {
+            answer.add(q.poll());
         }
-        return answer;
+        return answer.stream()
+            .mapToInt(Integer::intValue)
+            .toArray();
     }
 }
