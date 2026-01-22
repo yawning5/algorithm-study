ea22287a6537c3f15cb0b9ee5d2f410a8fec462a
yawning5 <yawning055@gmail.com>
2026-01-22 15:42:21 +0900

    [level 1] Title: K번째수, Time: 0.39 ms, Memory: 78.2 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.java
+++ 프로그래머스/1/42748. K번째수/K번째수.java
@@ -1,18 +1,13 @@
-import java.util.Arrays;
+import java.util.*;
 
 class Solution {
     public int[] solution(int[] array, int[][] commands) {
         int[] answer = new int[commands.length];
-        int i = 0;
-
-        for (int[] com : commands) {
-            int[] temp = new int[com[1] - com[0] + 1];
-            System.arraycopy(array, com[0] - 1, temp, 0, com[1] - com[0] + 1);
-            Arrays.sort(temp);
-            answer[i] = temp[com[2] - 1];
-            i++;
+        for (int i = 0; i < commands.length; i++) {
+            int[] cur = Arrays.copyOfRange(array, commands[i][0] - 1, commands[i][1]);
+            Arrays.sort(cur);
+            answer[i] = cur[commands[i][2] - 1];
         }
-
         return answer;
     }
 }
--- 프로그래머스/1/42748. K번째수/README.md
+++ 프로그래머스/1/42748. K번째수/README.md
@@ -1,10 +1,10 @@
 # [level 1] K번째수 - 42748 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42748?language=javascript) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42748?language=java) 
 
 ### 성능 요약
 
-메모리: 33.4 MB, 시간: 0.09 ms
+메모리: 78.2 MB, 시간: 0.39 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 07월 04일 20:22:35
+2026년 01월 22일 15:42:18
 
 ### 문제 설명
 
