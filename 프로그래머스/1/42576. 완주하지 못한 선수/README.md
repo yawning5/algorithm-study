0fb21ac7042a676e07c0c80411fcb35f3a9375d9
yawning5 <yawning055@gmail.com>
2026-05-26 21:25:20 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 44.31 ms, Memory: 102 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/README.md
+++ 프로그래머스/1/42576. 완주하지 못한 선수/README.md
@@ -1,10 +1,10 @@
 # [level 1] 완주하지 못한 선수 - 42576 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42576) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42576?language=java) 
 
 ### 성능 요약
 
-메모리: 98 MB, 시간: 105.45 ms
+메모리: 102 MB, 시간: 44.31 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 12월 31일 18:58:56
+2026년 05월 26일 21:25:14
 
 ### 문제 설명
 
--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -2,23 +2,17 @@ import java.util.*;
 
 class Solution {
     public String solution(String[] participant, String[] completion) {
-        // 지원자 배열을 다 돌면서 K = 참가자 이름, V = 참가자 수 Map 을 만듬
-        Map<String, Integer> all = new HashMap<>();
-        
-        for (String p : participant) {
-            all.merge(p, 1, Integer::sum);
+	String answer = "";
+	Map<String, Integer> man = new HashMap<String, Integer>();
+	for (String p : participant) {
+		man.merge(p, 1, Integer::sum);
+	}
+	for (String c : completion) {
+		man.computeIfPresent(c, (k, v) -> v - 1);
+	}
+	for (var eSet : man.entrySet()) {
+		if (eSet.getValue() != 0) return eSet.getKey();
         }
-        // 완료자 배열을 돌면서 V - 1 을 함
-        for (String c : completion) {
-            all.merge(c, -1, Integer::sum);
-        }
-        
-        // 완료자 배열을 돌면서 V != 0 인 참가자 이름을 뱉음
-        for (Map.Entry<String, Integer> entry : all.entrySet()) {
-            if (entry.getValue() != 0)
-                return entry.getKey();
-        }
-        
         return null;
     }
 }
