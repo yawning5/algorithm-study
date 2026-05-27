5790f12c042f86cfde1ad602df5e6c138c99cbee
yawning5 <yawning055@gmail.com>
2026-05-27 14:50:30 +0900

    [level 1] Title: 체육복, Time: 0.36 ms, Memory: 62.4 MB -BaekjoonHub

--- 프로그래머스/1/42862. 체육복/체육복.java
+++ 프로그래머스/1/42862. 체육복/체육복.java
@@ -1,39 +1,33 @@
-import java.util.Arrays;
+import java.util.*;
 
 class Solution {
     public int solution(int n, int[] lost, int[] reserve) {
-        int answer = n - lost.length;
+	int good = n - lost.length;
+	Set<Integer> res = new HashSet<>();
+	Arrays.sort(lost);
+	for (int i : reserve) res.add(i);
 
-        Arrays.sort(lost);
-        Arrays.sort(reserve);
+	for (int i = 0; i < lost.length; i++) {
+		if (res.contains(lost[i])) {
+			res.remove(lost[i]);
+			lost[i] = -1;
+			// 여분을 가져온 사람이 도난당한 경우를 처리할때 good 에 + 1 을 해주지 않았었음
+			good += 1;
+		}
+	}
 
-        int k = 0;
-
-        while (k < lost.length) {
-            if (Arrays.binarySearch(reserve, lost[k]) >= 0) {
-                answer++;
-                reserve[Arrays.binarySearch(reserve, lost[k])] = -2;
-                lost[k] = -5;
-            }
-            k++;
-        }
-
-        Arrays.sort(lost);
-        Arrays.sort(reserve);
-
-        for (int i = 0; i < lost.length; i++) {
-
-            if (Arrays.binarySearch(reserve, lost[i] - 1) >= 0) {
-                answer++;
-                reserve[Arrays.binarySearch(reserve, lost[i] - 1)] = -2;
-                Arrays.sort(reserve);
-            } else if (Arrays.binarySearch(reserve, lost[i] + 1) >= 0) {
-                answer++;
-                reserve[Arrays.binarySearch(reserve, lost[i] + 1)] = -2;
-                Arrays.sort(reserve);
-            }
-        }
-
-        return answer;
+	for (int l : lost) {
+		if (l > 0) {
+			if (res.contains(l -1)) {
+				res.remove(l - 1);
+				good += 1;
+			} else if (res.contains(l + 1)) {
+				res.remove(l + 1);
+				good += 1;
+			}
+		}
+	}
+		
+        return good;
     }
 }
