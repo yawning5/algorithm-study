ea0eb22e76e08e6db71c0031e1c9ace842fb2a5e
yawning5 <yawning055@gmail.com>
2026-06-16 14:39:37 +0900

    [level 2] Title: 전화번호 목록, Time: 206.64 ms, Memory: 119 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -1,15 +1,15 @@
 import java.util.*;
-import java.util.stream.*;
 
 class Solution {
     public boolean solution(String[] phone_book) {
+        boolean answer = true;
         Arrays.sort(phone_book);
-                
+        
         for (int i = 0; i < phone_book.length - 1; i++) {
             if (phone_book[i + 1].startsWith(phone_book[i])) {
-                return false;
+            return false;
             }
         }
-        return true;
+        return answer;
     }
 }
