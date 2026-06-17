8d69ccf8ec85d8361142bdba278881d9c944689d
yawning5 <yawning055@gmail.com>
2026-06-17 13:52:57 +0900

    [level 2] Title: 의상, Time: 1.16 ms, Memory: 74.1 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.java
+++ 프로그래머스/2/42578. 의상/의상.java
@@ -1,17 +1,17 @@
 import java.util.*;
-import java.util.stream.*;
 
 class Solution {
     public int solution(String[][] clothes) {
         int answer = 1;
-        // 카테고리 별 key 를 만들고 string, integer 맵에 저장해서 조합으로 계산
         Map<String, Integer> map = new HashMap<>();
-        for (String [] clothe : clothes) {
-            map.merge(clothe[1], 1, Integer::sum);
+        
+        for (String[] c : clothes) {
+            map.merge(c[1], 1, Integer::sum);
         }
-        for (Map.Entry<String, Integer> entry : map.entrySet()) {
-            answer *= entry.getValue() + 1;
+        for (Map.Entry<String, Integer> e : map.entrySet()) {
+            answer *= e.getValue() + 1;
         }
+        
         return answer - 1;
     }
 }
